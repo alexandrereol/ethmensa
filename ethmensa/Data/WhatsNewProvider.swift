9c40bc3823254d5a693d356502ffac7dd71319dd
Alexandre Reol <alexandre@reol.ch>
2025-03-26 20:11:59 +0100

    Update Onboarding, WhatsNew with Legi feature

--- ethmensa/Data/WhatsNewProvider.swift
+++ ethmensa/Data/WhatsNewProvider.swift
@@ -9,25 +9,15 @@ import WhatsNewKit
 struct WhatsNewProvider: WhatsNewCollectionProvider {
     var whatsNewCollection: WhatsNewCollection {
         WhatsNew(
-            version: "6.5",
+            version: "6.9",
             title: .init(
-                stringLiteral: String(localized: "WHATS_NEW_IN_VERSION_\("6.5")")
+                stringLiteral: String(localized: "WHATS_NEW_IN_VERSION_\("6.9")")
             ),
             features: [
                 .init(
-                    image: .init(systemName: "globe.europe.africa"),
-                    title: .init(.init(localized: "NEW_LANGUAGES")),
-                    subtitle: .init(.init(localized: "NEW_LANGUAGES_SUBTITLE"))
-                ),
-                .init(
-                    image: .init(systemName: "exclamationmark.circle"),
-                    title: .init(.init(localized: "BETTER_ALLERGEN_INFO")),
-                    subtitle: .init(.init(localized: "BETTER_ALLERGEN_INFO_SUBTITLE"))
-                ),
-                .init(
-                    image: .init(systemName: "line.3.crossed.swirl.circle"),
-                    title: .init(.init(localized: "VARIOUS_UI_AND_ANMIATION_IMPROVEMENTS")),
-                    subtitle: .init(.init(localized: "VARIOUS_UI_AND_ANMIATION_IMPROVEMENTS_SUBTITLE"))
+                    image: .init(systemName: "person.text.rectangle.fill"),
+                    title: .init(.init(localized: "ACCESS_LEGI_CARD_IN_APP")),
+                    subtitle: .init(.init(localized: "LEGI_CARD_SUBTITLE"))
                 )
             ],
             primaryAction: .init(
@@ -43,9 +33,7 @@ struct WhatsNewProvider: WhatsNewCollectionProvider {
     Text(String("Hello World!"))
         .sheet(
             whatsNew: .constant(
-                WhatsNewProvider().whatsNewCollection.first {
-                    $0.version == "6.5"
-                }
+                WhatsNewProvider().whatsNewCollection.first!
             )
         )
 }
