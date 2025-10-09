490b82031fd5814374551ddedd120b966733e125
Alexandre Reol <alexandre@reol.ch>
2025-10-09 14:28:56 +0200

    Bump version to 6.12

--- ethmensa/Views/SettingsView/CheckMarkView.swift
+++ ethmensa/Views/SettingsView/CheckMarkView.swift
@@ -11,13 +11,21 @@ struct CheckMarkView: View {
 
     var body: some View {
         if shown {
-            Text(text)
-                .badge(
-                    Text("\(Image(systemName: "checkmark"))")
-                        .foregroundColor(.accent)
-                )
+            LabeledContent {
+                Image(systemName: "checkmark")
+                    .foregroundColor(.accent)
+            } label: {
+                Text(text)
+            }
         } else {
             Text(text)
         }
+    }
+}
+
+#Preview {
+    List {
+        CheckMarkView(text: "Hello World!", shown: true)
+        CheckMarkView(text: "Hello World!", shown: false)
     }
 }
